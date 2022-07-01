3e7ca172c434776066566c083a6b3166f4459afb
AlexanderWei <13816490147@163.com>
2022-07-01 13:38:01 +0800

    引入Keyboard Actions键盘管理框架。

--- lib/application/support_file/common_header.dart
+++ lib/application/support_file/common_header.dart
@@ -6,6 +6,7 @@ export 'package:provider/provider.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import '../third_party/keyboard_actions/keyboard_actions.dart';
 
 export '../widgets/text_button.dart';
 export '../widgets/text_field.dart';
